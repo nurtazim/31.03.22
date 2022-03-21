4daebd7f99583a73931e82dcc0ed590e9a5e5977
nurtazim <bakytovnurtazim@gmail.com>
2022-03-21 23:10:54 +0600

    Diploy

--- babyshop_backend/settings.py
+++ babyshop_backend/settings.py
@@ -88,12 +88,6 @@ WSGI_APPLICATION = 'babyshop_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
